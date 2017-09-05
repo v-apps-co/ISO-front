8c7c4b93956d4f6ac8f11d35b9e99ee77857c99f
sherief <sherif.abdelmgeed@alfarisict.com>
2017-09-05 12:28:40 +0200

    - knowledge management

--- app/js/controllers/knowledge_management/knowledge_management.js
+++ app/js/controllers/knowledge_management/knowledge_management.js
@@ -1,12 +1,7 @@
-app.controller('knowledgeManagementController', ['$scope','$translate',
-    function ($scope, $translate) {
+app.controller('knowledgeManagementController', ['$scope',
+    function ($scope) {
 
         console.log('Knowledge Controller');
-        $scope.addKnowledge = $translate.instant("page.knowledgeManagement.KNOWLEDGE_NAME");
-        $scope.dept = $translate.instant("page.knowledgeManagement.KNOWLEDGE_DEPT");
-        $scope.class = $translate.instant("page.knowledgeManagement.KNOWLEDGE_CLASS");
-        $scope.resp = $translate.instant("page.knowledgeManagement.KNOWLEDGE_RESP");
-        $scope.status = $translate.instant("page.knowledgeManagement.KNOWLEDGE_STATUS");
 
         ////////////
 
